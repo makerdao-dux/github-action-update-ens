1b1bf8361c2ead549c13e89b0b759bf52facd634
Tyler Sorensen <tyler@dux.makerdao.network>
2023-02-10 22:02:19 -0800

    start writing action

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,31 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import * as core from '@actions/core';
+import ethers from 'ethers';
+import {
+  DefenderRelayProvider,
+  DefenderRelaySigner
+} from 'defender-relay-client/lib/ethers';
+// @ts-ignore
+import ENS, { getEnsAddress } from '@ensdomains/ensjs';
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const NAME = core.getInput('name');
+    const KEY = core.getInput('key');
+    const VALUE = core.getInput('value');
+    const DEFENDER_API_KEY = core.getInput('defenderApiKey');
+    const DEFENDER_API_SECRET = core.getInput('defenderApiSecret');
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const credentials = { apiKey: DEFENDER_API_KEY, apiSecret: DEFENDER_API_SECRET };
+    const provider = new DefenderRelayProvider(credentials);
+    const signer = new DefenderRelaySigner(credentials, provider, { speed: 'fast' });
 
-    core.setOutput('time', new Date().toTimeString())
+    const ens = new ENS({ provider, ensAddress: getEnsAddress('5') }); //5 for mainnet
+    const tx = ens.name(NAME).setText();
+    console.log('tx', tx);
+
   } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+    if (error instanceof Error) core.setFailed(error.message);
   }
 }
 
-run()
+run();
