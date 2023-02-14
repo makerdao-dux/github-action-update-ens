a22862a585e229237ee73afe899d4db755ece091
Tyler Sorensen <tyler@dux.makerdao.network>
2023-02-13 22:33:29 -0800

    uncomment stuff

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import * as core from '@actions/core';
 //import ethers from 'ethers';
 import {
-  DefenderRelayProvider,
+  DefenderRelayProvider
   //DefenderRelaySigner
 } from 'defender-relay-client/lib/ethers';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import ENS, { getEnsAddress } from '@ensdomains/ensjs';
+// eslint-disable-next-line sort-imports
+import ENS, {getEnsAddress} from '@ensdomains/ensjs';
 
 async function run(): Promise<void> {
   try {
@@ -15,21 +17,23 @@ async function run(): Promise<void> {
     const DEFENDER_API_KEY = core.getInput('defenderApiKey');
     const DEFENDER_API_SECRET = core.getInput('defenderApiSecret');
 
-    const credentials = { apiKey: DEFENDER_API_KEY, apiSecret: DEFENDER_API_SECRET };
+    const credentials = {
+      apiKey: DEFENDER_API_KEY,
+      apiSecret: DEFENDER_API_SECRET
+    };
     const provider = new DefenderRelayProvider(credentials);
     //const signer = new DefenderRelaySigner(credentials, provider, { speed: 'fast' });
-    core.setOutput('provider', provider);
-    // const ens = new ENS({ provider, ensAddress: getEnsAddress('5') }); //5 for goerli
-    // core.debug('ens');
-    // core.debug(ens);
-    // const text = await ens.name(NAME).getText('ipfs');
-    // core.setOutput('text', text);
-    // core.debug('text');
-    // core.debug(text);
-    // const tx = await ens.name(NAME).setText(NAME, KEY, VALUE);
-    // core.debug('tx');
-    // core.debug(tx);
 
+    const ens = new ENS({provider, ensAddress: getEnsAddress('5')}); //5 for goerli
+    core.debug('ens');
+    core.debug(ens);
+    const text = await ens.name(NAME).getText('ipfs');
+    core.setOutput('text', text);
+    core.debug('text');
+    core.debug(text);
+    const tx = await ens.name(NAME).setText(NAME, KEY, VALUE);
+    core.debug('tx');
+    core.debug(tx);
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
   }
